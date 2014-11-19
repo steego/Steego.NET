7c04084bed4e4232cb093eb2a93e06ef79f058e4
Steve Goguen <steve.goguen@alliedbuilding.com>
2014-11-19 10:14:44 -0500

    Added Steego.LinqPad package to bundle

---
+++ Steego.LinqPad/Profiling/ProfilingExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Steego.LinqPad.Profiling {
+  public static class ProfilingExtensions {
+    public static void Profile<T>(this IEnumerable<T> sequence, int interval = 1000) {
+      var watch = new System.Diagnostics.Stopwatch();
+      watch.Start();
+      var count = 0;
+      foreach(var item in sequence) {
+        count += 1;
+        if(watch.ElapsedMilliseconds >= interval) {
+          Console.WriteLine("Time: {0}, Count: {1}", DateTime.Now, count);
+          count = 0;
+          watch.Restart();
+        }
+      }
+      Console.WriteLine("Profile all done");
+    }
+  }
+}
--- release.bat
+++ release.bat
@@ -1,2 +1,3 @@
 msbuild Steego.Net.sln /m /t:rebuild /p:Configuration=Release
 .nuget\nuget pack Steego.Conversions.nuspec
+.nuget\nuget pack Steego.LinqPad.nuspec
