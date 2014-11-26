11e786392d84ac8af12c3808f09e7e8d983bfd1a
Steve Goguen <steve.goguen@alliedbuilding.com>
2014-11-26 17:19:09 -0500

    Updated LinqPad Profile to return IEnumerable<T> instead of being terminal

--- Steego.LinqPad/Profiling/ProfilingExtensions.cs
+++ Steego.LinqPad/Profiling/ProfilingExtensions.cs
@@ -5,19 +5,20 @@ using System.Text;
 
 namespace Steego.LinqPad.Profiling {
   public static class ProfilingExtensions {
-    public static void Profile<T>(this IEnumerable<T> sequence, int interval = 1000) {
+    public static IEnumerable<T> Profile<T>(this IEnumerable<T> sequence, string name, int interval = 1000) {
       var watch = new System.Diagnostics.Stopwatch();
       watch.Start();
       var count = 0;
       foreach(var item in sequence) {
         count += 1;
+        yield return item;
         if(watch.ElapsedMilliseconds >= interval) {
-          Console.WriteLine("Time: {0}, Count: {1}", DateTime.Now, count);
+          Console.WriteLine("Name: {0}, Time: {1}, Count: {2}", name, DateTime.Now, count);
           count = 0;
           watch.Restart();
         }
       }
-      Console.WriteLine("Profile all done");
+      Console.WriteLine("Name: {0}, Profile all done", name);
     }
   }
 }
--- release.bat
+++ release.bat
@@ -1,3 +1,4 @@
+del *.nupkg
 msbuild Steego.Net.sln /m /t:rebuild /p:Configuration=Release
 .nuget\nuget pack Steego.Conversions.nuspec
 .nuget\nuget pack Steego.LinqPad.nuspec
