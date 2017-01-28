3cdf7beaf85f68426b0e730837f2ac6402ac3a62
Steve Goguen <steve.goguen@alliedbuilding.com>
2017-01-28 09:18:35 -0500

    Storing Linq Extension and Reflection code

---
+++ src/Steego.LinqPad/LinqExtensions.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Steego.Conversions {
+    public enum SortDirection {
+        Ascending, Descending
+    }
+
+    public struct SortParameter {
+        public string Column;
+        public SortDirection Direction;
+    }
+
+    public static class LinqExtensions {
+
+        private static SortParameter ParseOrderBy(string orderBy) {
+            if(orderBy == null)
+                throw new ArgumentNullException(nameof(orderBy));
+            var orderByParts = orderBy.Split(' ');
+
+            var orderByColumn = orderByParts[0];
+            var orderByDirection = SortDirection.Ascending;
+            if(orderByParts.Length > 1 && orderByParts[1].ToLower() == "desc") {
+                orderByDirection = SortDirection.Descending;
+            }
+            return new SortParameter() { Column = orderByColumn, Direction = orderByDirection };
+        }
+
+        /// <summary>
+        /// Used to perform typed and dynamic order by.
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="orderBy">The full order by expression, e.g. AuthorName DESC</param>
+        public static IQueryable<T> SortBy<T>(this IQueryable<T> table, string orderBy) {
+            if(String.IsNullOrEmpty(orderBy))
+                return table;
+
+            // Parse order by            
+            var orderByColumn = string.Empty;
+            var sortParameter = ParseOrderBy(orderBy);
+
+            return table.SortBy(sortParameter.Column, sortParameter.Direction);
+        }
+
+        public static IQueryable<T> SortBy<T>(this IQueryable<T> table, string orderByColumn, SortDirection orderByDirection) {
+
+            var TypeInfo = new TypeInfo<T>();
+
+            if(orderByColumn.IsBlank()) {
+                return table;
+            }
+            // Get sort results
+            var member = TypeInfo.Member(orderByColumn);
+            if(member == null) {
+                throw new Exception("Cannot sort column " + orderByColumn);
+            }
+            var propType = member.Type;
+
+            if(propType == typeof(DateTime?)) {
+                return GetDynamicSortResults<T, DateTime?>(table, orderByColumn, orderByDirection);
+            } else if(propType == typeof(int?)) {
+                return GetDynamicSortResults<T, int?>(table, orderByColumn, orderByDirection);
+            }
+
+            switch(Type.GetTypeCode(propType)) {
+                case TypeCode.Boolean:
+                    return GetDynamicSortResults<T, bool>(table, orderByColumn, orderByDirection);
+                case TypeCode.Byte:
+                    return GetDynamicSortResults<T, byte>(table, orderByColumn, orderByDirection);
+                case TypeCode.Char:
+                    return GetDynamicSortResults<T, char>(table, orderByColumn, orderByDirection);
+                case TypeCode.DateTime:
+                    return GetDynamicSortResults<T, DateTime>(table, orderByColumn, orderByDirection);
+                case TypeCode.Decimal:
+                    return GetDynamicSortResults<T, decimal>(table, orderByColumn, orderByDirection);
+                case TypeCode.Double:
+                    return GetDynamicSortResults<T, double>(table, orderByColumn, orderByDirection);
+                case TypeCode.Int16:
+                    return GetDynamicSortResults<T, Int16>(table, orderByColumn, orderByDirection);
+                case TypeCode.Int32:
+                    return GetDynamicSortResults<T, Int32>(table, orderByColumn, orderByDirection);
+                case TypeCode.Int64:
+                    return GetDynamicSortResults<T, Int64>(table, orderByColumn, orderByDirection);
+                case TypeCode.Single:
+                    return GetDynamicSortResults<T, float>(table, orderByColumn, orderByDirection);
+                case TypeCode.String:
+                    return GetDynamicSortResults<T, string>(table, orderByColumn, orderByDirection);
+                default:
+                    throw new Exception("Cannot sort column " + orderByColumn + " because of its type:" + member.Type.Name);
+            }
+
+        }
+
+        /// <summary>
+        /// Dynamically builds a Linq expression that sorts by a particular column
+        /// </summary>
+        /// <typeparam name="S">The return type of the Lambda</typeparam>
+        /// <param name="orderByColumn">The name of the order by column</param>
+        /// <param name="orderByDirection">The direction of the order by sort</param>
+        private IQueryable<T> GetDynamicSortResults<T, S>(IQueryable<T> table, string orderByColumn, SortDirection orderByDirection) {
+
+            ParameterExpression sortParameterExpression = Expression.Parameter(typeof(T), "e");
+            var sortPropertyExpression = Expression.PropertyOrField(sortParameterExpression, orderByColumn);
+
+            var sortExpression = Expression.Lambda<Func<T, S>>(sortPropertyExpression, sortParameterExpression);
+
+            if(orderByDirection != SortDirection.Descending) {
+                return table.OrderBy(sortExpression);
+            } else {
+                return table.OrderByDescending(sortExpression);
+            }
+        }
+
+        /// <summary>
+        /// Dynamically builds a Linq expression that sorts by a particular column
+        /// </summary>
+        /// <typeparam name="S">The return type of the Lambda</typeparam>
+        /// <param name="orderByColumn">The name of the order by column</param>
+        /// <param name="orderByDirection">The direction of the order by sort</param>
+        private IQueryable<T> GetDynamicSortResults<T, S>(IQueryable<T> table, string orderByColumn, string orderByDirection) {
+            return GetDynamicSortResults<T, S>(table, orderByColumn, ((orderByDirection.IfBlank("").ToLower() == "desc") ? SortDirection.Ascending : SortDirection.Descending));
+        }
+
+
+    }
+
+}
---
+++ src/Steego.LinqPad/ReflectionUtilities.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Steego.LinqPad {
+    public class ReflectionUtilities {
+        public interface ITypeInfo {
+            string Name { get; }
+            bool IsPrimitive { get; }
+            bool IsEnumerable { get; }
+            bool IsTypedEnumerable { get; }
+            IEnumerable<IMemberInfo> Members { get; }
+            IMemberInfo this[string name] { get; }
+        }
+
+        public interface IMemberInfo {
+            string Name { get; }
+            ITypeInfo TypeInfo { get; }
+            Type Type { get; }
+            Func<object, object> GetValue { get; }
+            MemberInfo ReflectionMemberInfo { get; }
+        }
+
+        public class TypeInfo<T> : ITypeInfo {
+
+            static TypeInfo() {
+                var ObjectType = typeof(T);
+                var IsString = ObjectType == typeof(string);
+                var Interfaces = ObjectType.GetInterfaces();
+
+                _Name = ObjectType.Name;
+                _IsPrimitive = (ObjectType.IsValueType | IsString);
+                _IsEnumerable = ObjectType.IsArray || typeof(IEnumerable).IsAssignableFrom(ObjectType);
+                _IsTypedEnumerable = _IsEnumerable && (
+                    from i in Interfaces
+                    where i.IsGenericType()
+                    where i.GetGenericTypeDefinition() == typeof(IEnumerable<>)
+                    select i).Any();
+                _Members = (
+                    from m in GetMemberGetters<T>()
+                    select new MemberInfo(m.Name, m.Type, m.Get)).ToArray();
+
+            }
+
+            public class MemberInfo : IMemberInfo {
+                public MemberInfo(string name, Type type, Func<object, object> getter) {
+                    Name = name;
+                    GetValue = getter;
+                    Type = type;
+                    TypeInfo = (ITypeInfo) Activator.CreateInstance(typeof(TypeInfo<>).MakeGenericType(type));
+                }
+
+                public System.Func<object, object> GetValue { get; }
+                public System.Reflection.MemberInfo ReflectionMemberInfo { get; }
+
+                public string Name { get; }
+
+                public ITypeInfo TypeInfo { get; }
+
+                public System.Type Type { get; }
+
+                //public System.Reflection.MemberInfo MemberInfo {get;}
+            }
+
+            private static readonly bool _IsEnumerable;
+            public bool IsEnumerable => _IsEnumerable;
+
+            private static readonly bool _IsPrimitive;
+            public bool IsPrimitive => _IsPrimitive;
+
+            private static readonly bool _IsTypedEnumerable;
+            public bool IsTypedEnumerable => _IsTypedEnumerable;
+
+            private static string _Name;
+            public string Name {
+                get {
+                    return _Name;
+                }
+            }
+
+            private static IMemberInfo[] _Members;
+            public System.Collections.Generic.IEnumerable<IMemberInfo> Members {
+                get {
+                    return _Members;
+                }
+            }
+
+            public IMemberInfo this[string name] {
+                get {
+                    var result = (
+                        from m in Members
+                        where m.Name == name
+                        select m).FirstOrDefault();
+                    if(result == null) {
+                        result = new MemberInfo(name, typeof(object), (o) => null);
+                    }
+                    return result;
+                }
+            }
+        }
+
+        public struct MemberGetter {
+            public string Name;
+            public Type Type;
+            public Func<object, object> Get;
+
+            public bool IsEnumerable {
+                get {
+                    if(Type.IsArray) {
+                        return true;
+                    }
+                    if(Type.GetInterface("IEnumerable") != null) {
+                        return true;
+                    }
+                    if(Type.HasElementType) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        }
+
+        public MemberGetter GetMemberGetter<T>(string name) {
+            var Found = (
+                from m in GetMemberGetters<T>()
+                where m.Name == name
+                select m).ToArray();
+
+            if(Found.Any()) {
+                return Found.FirstOrDefault();
+            } else {
+                var Getter = new MemberGetter();
+                Getter.Name = name;
+                Getter.Type = typeof(object);
+                Getter.Get = (o) => null;
+                return Getter;
+            }
+
+        }
+
+        public MemberGetter[] GetMemberGetters(Type ObjectType) {
+            try {
+                var Flags = BindingFlags.Public | BindingFlags.Instance;
+                var Properties = (
+                    from p in ObjectType.GetProperties((System.Reflection.BindingFlags) Flags)
+                    where p.IsSpecialName == false
+                    where p.GetIndexParameters().Length == 0 && p.CanRead && p.IsSpecialName == false
+                    let param = E.Parameter(typeof(object), "x")
+                    let cparam = E.Convert(param, ObjectType)
+                    let IsType = E.TypeIs(param, ObjectType)
+                    let GetProp = E.Convert(E.Property(cparam, p), typeof(object))
+                    let Check = E.Lambda<Func<object, object>>(E.Condition(IsType, GetProp, E.Constant(null)), param).Compile()
+                    select new MemberGetter {
+                        Name = p.Name,
+                        Type = p.PropertyType,
+                        Get = Check
+                    }).ToArray();
+
+                var Fields = (
+                    from f in ObjectType.GetFields((System.Reflection.BindingFlags) Flags)
+                    where f.IsPublic && f.IsSpecialName == false
+                    let param = E.Parameter(typeof(object), "x")
+                    let cparam = E.Convert(param, ObjectType)
+                    let IsType = E.TypeIs(param, ObjectType)
+                    let GetField = E.Convert(E.Field(cparam, f), typeof(object))
+                    let Check = E.Lambda<Func<object, object>>(E.Condition(IsType, GetField, E.Constant(null)), param).Compile()
+                    select new MemberGetter {
+                        Name = f.Name,
+                        Type = f.FieldType,
+                        Get = Check
+                    }).ToArray();
+
+                return Fields.Union(Properties).ToArray();
+
+            } catch(Exception ex) {
+                throw new Exception("Error getting getters for type: {0}".Fill(ObjectType.Name), ex);
+            }
+        }
+
+        public MemberGetter[] GetMemberGetters<T>() {
+            var ObjectType = typeof(T);
+            try {
+                var Flags = BindingFlags.Public | BindingFlags.Instance;
+                //INSTANT C# TODO TASK: Lambda expressions cannot be assigned to 'var':
+                var Properties =
+                    from p in ObjectType.GetProperties(Flags)
+                    where p.IsSpecialName == false
+                    where p.GetIndexParameters().Length == 0 && p.CanRead && p.IsSpecialName == false
+                    let param = E.Parameter(ObjectType, "x")
+                    let GetProp = E.Lambda<Func<T, object>>(E.Convert(E.Property(param, p), typeof(object)), param).Compile()
+                    select new MemberGetter {
+                        Name = p.Name,
+                        Type = p.PropertyType,
+                        Get = (o) => ((o is T) ? GetProp(o) : null)
+                    };
+
+                //INSTANT C# TODO TASK: Lambda expressions cannot be assigned to 'var':
+                var Fields =
+                    from f in ObjectType.GetFields(Flags)
+                    where f.IsPublic && f.IsSpecialName == false
+                    let param = E.Parameter(ObjectType, "x")
+                    let GetField = E.Lambda<Func<T, object>>(E.Convert(E.Field(param, f), typeof(object)), param).Compile()
+                    select new MemberGetter {
+                        Name = f.Name,
+                        Type = f.FieldType,
+                        Get = (o) => ((o is T) ? GetField((T) o) : null)
+                    };
+
+                return Fields.Union(Properties).ToArray();
+
+            } catch(Exception ex) {
+                throw new Exception("Error getting getters for type: {0}".Fill(ObjectType.Name), ex);
+            }
+        }
+
+        public object GetPropertyValue(object o, PropertyInfo p) {
+            try {
+                return p.GetValue(o, null);
+            } catch(Exception ex) {
+                return ex;
+            }
+        }
+
+        public interface ITypeReader {
+            IFieldReader[] Fields { get; }
+        }
+
+        public interface IFieldReader {
+            string Name { get; }
+            string Title { get; }
+            bool IsSortable { get; }
+            Type Type { get; }
+            object GetValue(object o);
+        }
+
+
+        public class TypeReader<T> : ITypeReader {
+            private static FieldReader[] _fields;
+
+            static TypeReader() {
+                _fields =
+                    from p in TypeDescriptor.GetProperties(typeof(T)).OfType<PropertyDescriptor>()
+                    where p.PropertyType.IsSerializable == true || p.PropertyType == typeof(XElement)
+                    select p;
+
+
+            }
+
+            public ITypeReader.IFieldReader[] Fields {
+                get {
+                    return _fields;
+                }
+            }
+
+            public class FieldReader : ITypeReader.IFieldReader {
+
+                private PropertyDescriptor p;
+
+                public FieldReader(PropertyDescriptor p) {
+                    this.p = p;
+                }
+
+                public object GetValue(object o) {
+                    return p.GetValue(o);
+                }
+
+                public string Name {
+                    get {
+                        return p.Name;
+                    }
+                }
+
+                public string Title {
+                    get {
+                        return p.Name.Replace("_", " ");
+                    }
+                }
+
+                public System.Type Type {
+                    get {
+                        return p.PropertyType;
+                    }
+                }
+
+                public bool IsSortable {
+                    get {
+                        return true;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
